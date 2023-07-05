e48522b2c418b3f6d0c8d6c7c7ade17a393b26d3
Fontr <fontrfontr@gmail.com>
2023-07-05 14:04:26 +0200

    56

--- SSJ/Assets/Scripts/test.cs
+++ SSJ/Assets/Scripts/test.cs
@@ -7,12 +7,12 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //∆≈Õﬂ
+        //123
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Syrex
+        //321
     }
 }
