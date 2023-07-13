10209a878d1b826c80cf519a09afe7fb92ced07d
SyrexKek <seryj002@mail.ru>
2023-07-14 01:03:10 +0200

    EnemyDone

--- SSJ/Assets/Scripts/Enemies/SimpleEnemy.cs
+++ SSJ/Assets/Scripts/Enemies/SimpleEnemy.cs
@@ -5,16 +5,21 @@ using UnityEngine.AI;
 
 public class SimpleEnemy : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    //private Rigidbody2D rb;
     private EnemyClass enemy;
 
-    public float viewDist = 15.0f;
+    public float viewDist = 15.0f; // Дальность обзора
+
+    public float PRange = 3f; // Радиус патрулирования
+    public float startWaitTime = 3f; // Время ожидания в точке патрулирования
+    private float timeBeforePatrol = 5f;
 
     //====================================================================================
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        //rb = GetComponent<Rigidbody2D>();
         enemy = GetComponent<EnemyClass>();
+        enemy.LastPlayerPos = this.transform.position;
     }
     //====================================================================================
 
@@ -22,14 +27,27 @@ public class SimpleEnemy : MonoBehaviour
     //====================================================================================
     void FixedUpdate()
     {
-        float PLdistance = Vector2.Distance(transform.position, enemy.player.transform.position);
-        
-        if (PLdistance < viewDist)
+        enemy.FollowPlayer(viewDist);
+
+        if (Vector2.Distance(this.transform.position, enemy.LastPlayerPos) < 0.4f)
         {
-            enemy.FollowPlayer(viewDist);
+            enemy.PlayerFound = false;
         }
-        
-        
+
+        if (enemy.PlayerFound)
+        {
+            enemy.MoveToPoint(enemy.LastPlayerPos);
+            timeBeforePatrol = 5f;
+        }
+
+        if (enemy.PlayerFound == false && enemy.EnemyActive)
+        {
+            if (timeBeforePatrol <= 0f)
+            {
+                enemy.Patrol(PRange, startWaitTime);
+            }
+            else { timeBeforePatrol -= Time.fixedDeltaTime; }
+        }
     }
     //====================================================================================
 }
